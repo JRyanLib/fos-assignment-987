ab8f8fdd531c560eb1dc103a531703060b18e252
Yau <RongQuan.Yau-1@student.uts.edu.au>
2016-05-29 21:20:31 +1000

    Removing generation of keys from master_view

--- skynet_part1/master/master_view.py
+++ skynet_part1/master/master_view.py
@@ -1,32 +1,5 @@
 import os
 
-from Crypto.Cipher import PKCS1_v1_5
-from Crypto.PublicKey import RSA
-from Crypto.Hash import SHA256
-
-# Protect private key with a passphrase
-password = "My Secret!"
-
-def generate_keys():
-    # Generate an 4096 RSA public and private key.
-    generate_key = RSA.generate(bits=4096, e=65537)
-
-    # Get the public key and export it
-    public_key = generate_key.publickey().exportKey()
-    print("Public Key: {}".format(public_key))
-
-    # Get the private key, store with passphrase and export it
-    password_private_key = generate_key.exportKey(passphrase = password)
-
-    # Store the export private key into der file type
-    key_file = open("myprivatekey.der", "wb")
-    key_file.write(password_private_key)
-    key_file.close()
-    print("Private Key: {}".format(password_private_key))
-
-# Testing for generate_keys
-generate_keys()
-
 def decrypt_valuables(f):
     # TODO: For Part 2, you'll need to decrypt the contents of this file
     # The existing scheme uploads in plaintext
