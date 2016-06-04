ca1e4b6be241a54a580c5a6b64c516e44d729425
JRyanLib <john.r.libunao@student.uts.edu.au>
2016-06-04 17:36:54 +1000

    Opening file for aes key
    Lines 19-21: New code written to replace old one. Old code opened
    private key and decrypted the secrets file with this key. New code
    decrypts the aes key file instead.
    
    Minor variable name changes to avoid confusion. Added parameter fn to
    help locate the aes key file.
    
    Lines 24-30: Commented out old code to work on later. Used to make code
    simpler and allow easier testing for checking that aes key is correct
    
    Line 31: Added line to put a break in code execution for debugging

--- skynet_part1/master_view.py
+++ skynet_part1/master_view.py
@@ -8,7 +8,7 @@ from Crypto.Cipher import AES
 # Protect private key with a passphrase
 password = "My Secret!"
 
-def decrypt_valuables(f):
+def decrypt_valuables(f, fn):
     # TODO: For Part 2, you'll need to decrypt the contents of this file
     # The existing scheme uploads in plaintext
     # As such, we just convert it back to ASCII and print it out
@@ -16,19 +16,19 @@ def decrypt_valuables(f):
     print("File: {}".format(f))
 
     key = open("myprivatekey.txt", "r").read()
-    decryption_key = RSA.importKey(key, passphrase=password)
-    priv_key = PKCS1_v1_5.new(decryption_key)
-    decrypted_rsa_key = decryption_key.decrypt(f)
-    print(decrypted_rsa_key)
+    rsa_key = RSA.importKey(key, passphrase=password)
+    encr_aes_key = open(os.path.join("pastebot.net", fn + ".AES.key"), "rb").read()
+    aes_key = rsa_key.decrypt(encr_aes_key)
 
     # Extracts iv from data:f
-    iv = f[:16]
-    print(iv)
-    cipher = AES.new(f[:16], AES.MODE_CBC, iv)
-    paddedData = cipher.decrypt(f[16:])
-    unpaddedData = ANSI_X923_unpad(paddedData, 16)
-    print(unpaddedData)
-    return unpaddedData
+    # iv = f[:16]
+    # print(iv)
+    # cipher = AES.new(f[:16], AES.MODE_CBC, iv)
+    # paddedData = cipher.decrypt(f[16:])
+    # unpaddedData = ANSI_X923_unpad(paddedData, AES.block_size)
+    # print(unpaddedData)
+    # return unpaddedData
+    print("test")
 
     # # decoded_text = str(f, 'ascii')
     # # print(decoded_text)
@@ -49,4 +49,4 @@ if __name__ == "__main__":
         print("The given file doesn't exist on pastebot.net")
         sys.exit(1)
     f = open(os.path.join("pastebot.net", fn), "rb").read()
-    decrypt_valuables(f)
+    decrypt_valuables(f, fn)
