408902b0977d41eb32b50c6f3ccea5745583a10f
Yau <RongQuan.Yau-1@student.uts.edu.au>
2016-06-04 14:25:02 +1000

    Coding for decrypting valuables
    Line 1, 5: Import sys to make line 50 working and AES from Cipher file
    Line 8-9: Insert password to obtain myprivatekey text file
    Line 21-31: Try to decrypt valuables f with RSA private key first. Then
    unpadding f so it be decrypt with AES encryption key.
    Line 36-44: Obtain code from tutorial code example for unpadding in CBC
    mode.

--- skynet_part1/master_view.py
+++ skynet_part1/master_view.py
@@ -1,27 +1,52 @@
-import os
+import os, sys
 
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5
+from Crypto.Cipher import AES
+
+
+# Protect private key with a passphrase
+password = "My Secret!"
 
 def decrypt_valuables(f):
     # TODO: For Part 2, you'll need to decrypt the contents of this file
     # The existing scheme uploads in plaintext
     # As such, we just convert it back to ASCII and print it out
 
+    print("File: {}".format(f))
+
     key = open("myprivatekey.txt", "r").read()
     decryption_key = RSA.importKey(key, passphrase=password)
     priv_key = PKCS1_v1_5.new(decryption_key)
-    decrypt_msg = decryption_key.decrypt(f)
-    print(decrypt_msg)
+    decrypted_rsa_key = decryption_key.decrypt(f)
+    print(decrypted_rsa_key)
 
-    # decoded_text = str(f, 'ascii')
-    # print(decoded_text)
+    # Extracts iv from data:f
+    iv = f[:16]
+    print(iv)
+    cipher = AES.new(f[:16], AES.MODE_CBC, iv)
+    paddedData = cipher.decrypt(f[16:])
+    unpaddedData = ANSI_X923_unpad(paddedData, 16)
+    print(unpaddedData)
+    return unpaddedData
 
+    # # decoded_text = str(f, 'ascii')
+    # # print(decoded_text)
+
+def ANSI_X923_unpad(m, pad_length):
+    # The last byte should represent the number of padding bytes added
+    required_padding = m[-1]
+    # Ensure that there are required_padding - 1 zero bytes
+    if m.count(bytes([0]), -required_padding, -1) == required_padding - 1:
+        return m[:-required_padding]
+    else:
+        # Raise an exception in the case of an invalid padding
+        raise AssertionError("Padding was invalid")
 
 if __name__ == "__main__":
     fn = input("Which file in pastebot.net does the botnet master want to view? ")
     if not os.path.exists(os.path.join("pastebot.net", fn)):
         print("The given file doesn't exist on pastebot.net")
-        os.exit(1)
+        sys.exit(1)
     f = open(os.path.join("pastebot.net", fn), "rb").read()
     decrypt_valuables(f)
